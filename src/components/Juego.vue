2aeb46610b494ccd9f3d35466bf27f24ac39b43e
halg830 <halg830@gmail.com>
2023-08-29 17:52:56 -0500

    bendito sea chat gpt

--- src/components/Juego.vue
+++ src/components/Juego.vue
@@ -1,22 +1,76 @@
 <script setup>
+import {ref, computed} from 'vue'
+import error1 from '/src/assets/error1.png'
+import error2 from '/src/assets/error2.png'
+
 const alfabeto = Array.from({ length: 26 }, (_, index) => String.fromCharCode(65 + index))
+
+const palabra = "verde"
+
+const descubiertas = ref([""])
+
+const errores = ref(0)
+
+const comprobar = (letra)=>{
+  if(descubiertas.value.includes(letra)) return
+
+  if(palabra.includes(letra.toLowerCase())){
+    descubiertas.value.push(letra)
+    event.target.setAttribute("disabled", "true")
+    return
+  }
+
+  errores.value+=1
+}
+
+
+
+const buscar = computed(()=>{
+  return (letra)=>{
+    const prueba = descubiertas.value.find(e=> String(e)===String(letra.toUpperCase()))
+      if(prueba) return letra
+      return ""
+  } 
+})
+
 
 </script>
 
 <template>
   <div>
     <div>
-        <div>
-            <button v-for="(item, index) in alfabeto" :key="index">{{ item }}</button>
+      <div class="palabra">
+        <div v-for="(letra, index) in palabra" :key="index" class="letra">
+          <div class="cuadrito">
+            <h1 >{{buscar(letra) }}</h1>
+          </div>
+
+          <hr>
         </div>
+      </div>
+
+      <div>
+        <button v-for="(item, index) in alfabeto" :key="index" @click="comprobar(item)">{{ item }}</button>
+      </div>
     </div>
     <div>
-      
+      <img :src="error1" alt="">
     </div>
   </div>
 </template>
 
 <style scoped>
+.palabra {
+  display: flex;
+}
 
+.letra {
+  margin: 10px;
+}
 
+.cuadrito {
+  width: 20px;
+  height: 20px;
+  border: 1px solid black;
+}
 </style>
