9d5ce605ee5a3ed17b2eadfef1351f1ceae16a14
halg830 <halg830@gmail.com>
2023-09-12 12:36:26 -0500

    fusion de componentes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 // import { createApp } from 'vue'
 import './style.css'
 // import App from './App.vue'
-import Juego from './components/Juego.vue'
 
 // createApp(App).mount('#app')
-createApp(Juego).mount('#juego')
+
 
 import { createApp } from 'vue'
 import { Quasar } from 'quasar'
